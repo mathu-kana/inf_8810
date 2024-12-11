921b9e3539ec29cf339262823f4646361ed6cc33
zathu <mathura.kana@hotmail.com>
2024-12-11 14:08:28 -0500

    input repertoire path + sample_size

--- pretraitement.py
+++ pretraitement.py
@@ -15,11 +15,11 @@ def preprocess_reviews(recipes_file, reviews_file, output_reviews_file):
     filtered_reviews = reviews_data[reviews_data['recipe_id'].isin(recipe_ids)]
     filtered_reviews.to_csv(output_reviews_file, index=False) # sauvegarde
 
-    print(f"Nombre de rangées dans 'interactions.csv': {len(filtered_reviews)}.")
-    print(f"Sauvegarde de: \n{output_reviews_file}.")
+    print(f"\nNombre de rangées dans 'interactions.csv': {len(filtered_reviews)}.")
+    print(f"Sauvegarde de: {output_reviews_file}.")
     return
 
-def preprocess_recipes(repertoire_path, sample_size=100):
+def preprocess_recipes(repertoire_path, sample_size=None):
     # chemins aux fichiers
     input_recipes_file = os.path.join(repertoire_path, 'RAW_recipes.csv')
     output_recipes_file = os.path.join(repertoire_path, 'recipes.csv')
@@ -36,16 +36,19 @@ def preprocess_recipes(repertoire_path, sample_size=100):
             .str.replace(r"[\"']", "", regex=True)  # enlève les guillemets anglais simples et doubles
             .str.replace(", ", "|")  # remplace virgules avec |
         )
-    #TODO remove, temporaire pour sauver du temps
-    # échantillonnage
-    if len(data) <= sample_size:
-        data.to_csv(output_recipes_file, index=False) # pas d'échantillonnage
+        
+    # échantillonnage si nécessaire
+    if sample_size is None:
+        data.to_csv(output_recipes_file, index=False)
+        print(f"\nPas d'échantillonage. Sauvegarde de {output_recipes_file}")
     else:
-        sampled_data = data.sample(n=sample_size, random_state=99) # échantillonner 100 recettes/rangées
-
-        # sauvegarder 'recipes.csv'
-        sampled_data.to_csv(output_recipes_file, index=False)
-        print(f"Échantillon de {sample_size} recettes et sauvegarde de:\n{output_recipes_file}")
+        if len(data) <= sample_size:
+            data.to_csv(output_recipes_file, index=False)
+            print(f"\nPas d'échantillonage. sample_size inférieur au nombre de rangées dans RAW_recipes.csv. Sauvegarde de {output_recipes_file}")
+        else:
+            sampled_data = data.sample(n=sample_size, random_state=99)
+            sampled_data.to_csv(output_recipes_file, index=False)
+            print(f"\nÉchantillon de {sample_size} rangées sauvegardé dans:\n{output_recipes_file}")
     
     # pretraitement de RAW_interactions.csv pour garder les reviews pertinents à recipes.csv
     preprocess_reviews(output_recipes_file, input_reviews_file, output_reviews_file)
@@ -53,12 +56,36 @@ def preprocess_recipes(repertoire_path, sample_size=100):
     return
 
 #inputs
-#TODO changer les paths
 # Les fichiers input doivent être dans le repertoire d'importation de Neo4j
 #   - 'RAW_recipes.csv'
 #   - 'RAW_interactions.csv
 
-# chemin du repertoire d'importation de Neo4j
-repertoire_path = '/Users/mathu/Desktop/uqam/Trimestre_4-5/inf8810/import'
+#TODO change à un input() pour demander le chemin et sample_size à l'utilisateur
+while True:
+    repertoire_path = input("Entrez le chemin du répertoire contenant les fichiers CSV (repertoire d'importation Neo4j):\n ").strip()
+    if os.path.isdir(repertoire_path):
+        break
+    else:
+        print("Chemin invalide. Veuillez réessayer.")
 
-preprocess_recipes(repertoire_path, sample_size=100)
+while True:
+    try:
+        sample_size_input = input("Entrez la taille de l'échantillon ou laissez vide: ").strip()
+        if sample_size_input == "":
+            sample_size = None
+            break
+        sample_size = int(sample_size_input)
+        if sample_size > 0:
+            break
+        else:
+            print("Entrée invalide: entier positif requis. Veuillez réessayer.")
+    except ValueError:
+        print("Entrée invalide. Veuillez entrer un entier positif ou laisser vide.")
+
+
+# # chemin du repertoire d'importation de Neo4j
+# repertoire_path = '/Users/mathu/Desktop/uqam/Trimestre_4-5/inf8810/import'
+# sample_size = 100
+# # preprocess_recipes(repertoire_path)
+
+preprocess_recipes(repertoire_path, sample_size)
